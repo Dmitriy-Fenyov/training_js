5fa5948094b6839f491eba0ac079da4703cf9834
Dmitriy <rsp.fdrf@yandex.ru>
2022-07-03 18:37:03 +0300

    Выполнил задания по js с 25 по 34

---
+++ 25-expression-or-statement/finish.js
@@ -0,0 +1,39 @@
+/** ЗАДАЧА 25 - Выражение или инструкция
+ *
+ * Определите тип каждой конструкции JavaScript:
+ *  - выражение (expression)
+ *  - инструкция (statement)
+ *  - выражение-инструкция (expression statement)
+ */
+
+// expression statement
+15
+
+// statement
+const myObject = {
+  x: 10,
+  y: true,
+}
+
+// expression statement
+myObject.z = 'abc' // expression
+
+// expression statement
+delete myObject.x
+
+// statement
+let newVariable
+
+// expression statement
+newVariable = 30 + 5 // expression
+
+// expression statement
+console.log(newVariable)
+
+
+// statement
+if (newVariable > 10) {
+ 
+  // expression statement
+  console.log(`${newVariable} больше 10`) // newVariable is expression
+}
